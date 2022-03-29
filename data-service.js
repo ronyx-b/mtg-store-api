87a4c632d77d71e4948ca70a4c7e34b5fac59769
Rony Boscan <58662742+ronyx-b@users.noreply.github.com>
2022-03-28 20:09:52 -0400

    first commit

--- data-service.js
+++ data-service.js
@@ -8,6 +8,14 @@ dotenv.config();
 // mongoose.connect(process.env.MONGODB_CONN_STR);
 
 // Define schemas
+let addressSchema = new Schema({
+  "street": String,
+  "city": String,
+  "province": String,
+  "postal": String
+});
+let Address;
+
 let userSchema = new Schema({
   "email": {
     "type": String,
@@ -15,13 +23,16 @@ let userSchema = new Schema({
   },
   "first_name": String,
   "last_name": String,
-  "address": String,
-  "city": String,
-  "postal": String,
+  "address": [{
+    "street": String,
+    "city": String,
+    "province": String,
+    "postal": String
+  }],
   "password": String,
-  "role": {
-    "type": String,
-    "default": "customer"
+  "isAdmin": {
+    "type": Boolean,
+    "default": false
   }
 });
 let User;
@@ -40,14 +51,38 @@ let productSchema = new Schema({
 });
 let Product;
 
+let orderSchema = new Schema({
+  "user_id": String,
+  "date": Date,
+  "address": {
+    "street": String,
+    "city": String,
+    "province": String,
+    "postal": String
+  },
+  "products": [
+    {
+      "prodType": String,
+      "prod_id": String,
+      "name": String,
+      "cardSet": String,
+      "qty": Number,
+      "price": Number
+    }
+  ]
+});
+let Order;
+
 module.exports.connect = async () => {
   let db = mongoose.createConnection(process.env.MONGODB_CONN_STR, { useUnifiedTopology: true });
   db.on('error', (err) => {
     throw err;
   });
   db.once('open', () => {
+    Address = db.model("address", addressSchema)
     User = db.model("users", userSchema);
     Product = db.model("products", productSchema);
+    Order = db.model("orders", orderSchema);
     return;
   })
 }
@@ -71,3 +106,54 @@ module.exports.addProduct = async (data) => {
 module.exports.getAllProducts = async () => {
   return Product.find().exec();
 }
+
+module.exports.registerUser = async (userData) => {
+  try {
+    existingUser = await User.findOne({ email: userData.email }).exec();
+    if(!existingUser) {
+      hashedPassword = await bcrypt.hash(userData.password, 10);
+      let data = {
+        email: userData.email,
+        first_name: userData.firstName,
+        last_name: userData.lastName,
+        address: [{
+          street: userData.address,
+          city: userData.city,
+          province: '',
+          postal: userData.postal
+        }],
+        password: hashedPassword,
+        isAdmin: false
+      };
+      let newUser = new User(data);
+      newUser.save((err) => {
+        if(err) {
+          throw `Error saving user: ${err}`;
+        } else {
+          console.log("The new user was saved to the users collection");
+          return;
+        }
+      });
+    } else {
+      throw "There is a user already registered with the given email";
+    }
+  } catch (err) {
+    throw `Error creating user: ${err}`;
+  }
+};
+
+module.exports.login = async (userData) => {
+  try {
+    let user = await User.findOne({ email: userData.email }).exec();
+    if (!user) {
+      throw 'no user found with that email';
+    } 
+    let match = await bcrypt.compare(userData.password, user.password);
+    if (match !== true) {
+      throw 'the password is incorrect';
+    }
+    return user;
+  } catch (err) {
+    throw `cant't log in: ${err}`;
+  }
+};
--- server.js
+++ server.js
@@ -1,19 +1,59 @@
 const express = require('express'); // create express server
 const app = express();
 const cors = require('cors'); // load cors package
+const dotenv = require('dotenv'); // ENV variables
+dotenv.config();
+const jwt = require('jsonwebtoken'); // JWT and Passport
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
 
 const dataService = require('./data-service.js')
+
+const HTTP_PORT = process.env.PORT || 8080;
 
 /* configure middleware: express built-in body parser, cors */
 app.use(express.json());
 app.use(cors());
 
-const HTTP_PORT = process.env.PORT || 8080;
+/* ****************************** JWT Configuration ****************************** */
+// JSON Web Token Setup
+let ExtractJwt = passportJWT.ExtractJwt;
+let JwtStrategy = passportJWT.Strategy;
 
+// Configure its options
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+
+jwtOptions.secretOrKey = process.env.JWT_SECRET;
+
+let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+  console.log('payload received', jwt_payload);
+
+  if (jwt_payload) {
+    next(null, {
+      _id: jwt_payload._id,
+      email: jwt_payload.email
+    });
+  } else {
+    next(null, false);
+  }
+});
+
+// tell passport to use our "strategy"
+passport.use(strategy);
+
+// add passport as application-level middleware
+app.use(passport.initialize());
+
+
+/* ****************************** Server Routes ****************************** */
+// Use React build
 app.use('/', express.static('build'));
 
+// Images access route
 app.use('/img', express.static('img'));
 
+// API Entry point
 app.get('/api', async (req, res) => {
   try {
     let msg = await dataService.listening();
@@ -23,6 +63,7 @@ app.get('/api', async (req, res) => {
   }
 });
 
+/* DELETE WHEN GOING TO IMPLEMENTATION --------------------------------------------------------------------------------------------------*/
 app.get('/api/add-prod', async (req, res) => {
   let data = {
     name: 'Kamigawa: Neon Dynasty - Bundle',
@@ -48,6 +89,38 @@ app.get('/api/products/', async (req, res) => {
     res.json({message: `there was an error: ${err}`});
   }
 });
+
+// Register new user
+app.post('/api/users', async (req, res) => {
+  try {
+    let formData = req.body;
+    await dataService.registerUser(formData);
+    res.json({success: true, message: 'new user registered'});
+  } catch (err) {
+    res.json({success: false, message: `Error: ${err}`})
+  }
+});
+
+// Login user - POST /api/user/login
+app.post('/api/user/login', async (req, res) => {
+  try {
+    let user = await dataService.login(req.body);
+    let tokenPayload = {
+      _id: user._id,
+      email: user.email,
+      isAdmin: user.isAdmin
+    };
+    let token = jwt.sign(tokenPayload, jwtOptions.secretOrKey);
+    res.json({message: "login successful", token: token})
+  } catch (err) {
+    res.status(422).json({message: err});
+  }
+});
+
+app.get('/api/user/account', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json({message: 'user authenticated'});
+});
+
 
 dataService.connect().then(() => {
   app.listen(HTTP_PORT, () => { console.log(`app listening on: ${HTTP_PORT}`); });
